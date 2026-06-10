55e615f8856cff22b1e9a176b7a1cb74ac2bf2a1
azizahnurrahma <azizahnurrahma06@gmail.com>
2026-06-10 14:20:02 +0700

    CRUD buku

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,14 +25,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Here you may define how you wish users to be authenticated for your Lumen
-        // application. The callback which receives the incoming request instance
-        // should return either a User instance or null. You're free to obtain
-        // the User instance via an API token or any other method necessary.
-
+        // Di sini Lumen langsung membaca Token dari Header Postman tanpa bantuan file config
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->header('Authorization')) {
+                $key = explode(' ', $request->header('Authorization'));
+                $token = $key[1] ?? '';
+                
+                // Mencari user di database yang memiliki api_token tersebut
+                return \App\Models\User::where('api_token', $token)->first();
             }
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,19 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () {
     return 'Hello World';
 });
+
+// --- FITUR LOGIN (Bisa diakses tanpa login) ---
 $router->post('api/login', 'AuthController@login');
+
+
+// --- FITUR CRUD BUKU (DIKUNCI: Harus Autentikasi/Login Dulu) ---
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('api/buku', 'BookController@index');
+    $router->post('api/buku', 'BookController@store');
+    $router->get('api/buku/{id}', 'BookController@show');
+    $router->put('api/buku/{id}', 'BookController@update');
+    $router->delete('api/buku/{id}', 'BookController@destroy');
+});
